4c12d8f2de4010aafed8e5ef786c3ed9279237fc
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-09-11 15:28:20 -0300

    feat: finish sign-in and sing-up cards

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,5 +1,101 @@
-export const SignUpCard = () => {
-  return (
-    <div>Sign Up Card</div>
-  )
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
+
+interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
 }
+
+export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>
+          Use you email or another service to continue
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={false}
+            value={email}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={false}
+            value={password}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Input
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            disabled={false}
+            value={confirmPassword}
+            placeholder="Confirm password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={false}>
+            Continue
+          </Button>
+        </form>
+
+        <Separator />
+
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            onClick={() => { }}
+            disabled={false}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FcGoogle className="size-5 absolute top-2.5 left-2.5" />
+            Continue with Google
+          </Button>
+
+          <Button
+            onClick={() => { }}
+            disabled={false}
+            variant="outline"
+            size="lg"
+            className="w-full relative"
+          >
+            <FaGithub className="size-5 absolute top-2.5 left-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+
+        <p className="text-xs text-muted-foreground">
+          Already have an account?
+          <span
+            onClick={() => setState("signIn")}
+            className="text-sky-700 hover:underline cursor-pointer"> Sign in</span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
