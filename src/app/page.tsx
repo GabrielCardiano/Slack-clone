e7b78c6ff564ed7976c9f2f35e8955f9efe29f34
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-09-16 14:06:41 -0300

    feat: add get user query and user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
 import { UserButton } from "@/features/auth/components/user-button";
-import { useAuthActions } from "@convex-dev/auth/react";
 
-export default function Home() {
-   const { signOut } = useAuthActions();
-   
+export default function Home() {   
    return (
       <div className="flex flex-col justify-center  items-center min-h-screen gap-4">
-         <h1>Logged in</h1>
          <UserButton />
       </div>
 
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,30 +1,48 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useCurrentUser } from "../hooks/use-current-user";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Loader, LogOut } from "lucide-react";
 
 
 export const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name } = data;
+  const avatarFallBack = name!.charAt(0).toUpperCase();
+
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
-        <Avatar className="size-10 hover:opacity-75 transition">
-          <AvatarImage />
-          <AvatarFallback>
-
+        <Avatar className="size-14 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="border bg-slack-purple-1/95 text-white text-xl">
+            {avatarFallBack}
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem>
-
+        <DropdownMenuItem onClick={() => signOut()} className="cursor-pointer h-10">
+          <LogOut className="size-4 mr-2" />
+          Log Out
         </DropdownMenuItem>
       </DropdownMenuContent>
 
