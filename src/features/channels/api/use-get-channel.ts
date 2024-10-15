5c6f54e9eeb1dce3bde13bc08390336599160762
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2024-10-15 00:33:51 -0300

    feat: add endpoint UPDAte /channel name and DELETE /channel. Add use-update-channel and use-remove-channel queries.

--- src/features/channels/api/use-get-channel.ts
+++ src/features/channels/api/use-get-channel.ts
@@ -2,11 +2,11 @@ import { useQuery } from "convex/react"
 import { api } from "@/_generated/api"
 import { Id } from "@/_generated/dataModel";
 
-interface UseGetChannelByIdProps {
+interface UseGetChannelProps {
   id: Id<"channels">;
 };
 
-export const useGetChannelById = ({ id }: UseGetChannelByIdProps) => {
+export const useGetChannel = ({ id }: UseGetChannelProps) => {
   const data = useQuery(api.channels.getById, { id });
   const isLoading = data === undefined;
   return { data, isLoading };
